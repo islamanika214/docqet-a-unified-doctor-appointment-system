d4f9a146a4fcd7f02c46f7fcdd113acbaa2a16b5
Anika Islam <nusratanika214@gmail.com>
2025-08-21 02:23:58 -0700

    Appointments of doctor

--- admin/src/pages/DoctorPages/DoctorAppointment.jsx
+++ admin/src/pages/DoctorPages/DoctorAppointment.jsx
@@ -1,4 +1,15 @@
+import { useContext, useEffect } from "react";
+import { DoctorContext } from "../../context/DoctorContext";
+
 const DoctorAppointment = () => {
+    const { dToken, appointments, getAppointments } = useContext(DoctorContext);
+
+    useEffect(() => {
+        if (dToken) {
+            getAppointments();
+        }
+    }, [dToken]);
+
     return <div></div>;
 };
 
